b9e39c62424c21f8e901cd014d01aeafaca85841
taotao.zhou <taotao.zhou@ly.com>
2026-03-11 17:46:51 +0800

    feat: 优化镜像

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# 根据 DAOKEENV 选择 JWT 接口地址
-if [ "${DAOKEENV:-}" = "product" ]; then
-    export JWT_URL="https://toca.17u.cn/open-api/auth/openclaw-jwt"
-else
-    export JWT_URL="https://toca.qa.17u.cn/open-api/auth/openclaw-jwt"
-fi
-
 mkdir -p /home/openclaw/.openclaw/
 if [ -f /home/openclaw/.openclaw/openclaw.json ]; then
   rm -rf /home/openclaw/.openclaw/openclaw.json
@@ -21,19 +14,13 @@ cp -rf /scripts/home/.profile /home/openclaw
 cp -rf /scripts/home/.cache /home/openclaw
 
 mkdir -p /home/openclaw/.openclaw/skills
-chown -R openclaw:openclaw /home/openclaw
-chown -R openclaw:openclaw /scripts
+chown openclaw:openclaw /home/openclaw/.openclaw
 
 # openclaw 用户只读目录
-chown -R root:openclaw /home/openclaw/.openclaw/openclaw.json
-chmod -R 750 /home/openclaw/.openclaw/openclaw.json
-chown -R root:openclaw /home/openclaw/.openclaw/skills
-chmod -R 750 /home/openclaw/.openclaw/skills
-
-# 启动工程效能进程
-#nohup gosu openclaw python /scripts/browser_daemon/browser_daemon.py &
-
-#exec gosu openclaw openclaw gateway --port "${OPENCLAW_GATEWAY_PORT}"
+chown root:openclaw /home/openclaw/.openclaw/openclaw.json
+chmod 750 /home/openclaw/.openclaw/openclaw.json
+#chown -R root:openclaw /home/openclaw/.openclaw/skills
+#chmod -R 750 /home/openclaw/.openclaw/skills
 
 mkdir -p /var/log/openclaw
 exec /usr/bin/supervisord
